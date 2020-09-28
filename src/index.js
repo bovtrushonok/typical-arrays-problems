e9fb5a2c355be1bd3d2296aef821b27efe842009
Marie Bovtramovich <m.abovt@mail.ru>
2020-09-28 21:42:18 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ exports.min = function min (array) {
 	let arr = array;
 	if ( arguments.length === 0 || arr.length === 0 ) return 0;
 	return arr.sort( (a, b) => a - b )[0];
-  
 }
 
 exports.max = function max (array) {
