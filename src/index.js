4ba02c3626ea6a4246a96e8c46a5ae5d86167b43
Marie Bovtramovich <m.abovt@mail.ru>
2020-09-28 21:38:49 +0300

    add: solutions add

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 
 exports.min = function min (array) {
-  return 0;
+	let arr = array;
+	if ( arguments.length === 0 || arr.length === 0 ) return 0;
+	return arr.sort( (a, b) => a - b )[0];
+  
 }
 
 exports.max = function max (array) {
-  return 0;
+	let arr = array;
+	if (arguments.length === 0 || arr.length === 0) return 0;
+	return arr.sort( (a, b) => b - a )[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	let arr = array;
+	if (arguments.length === 0 || arr.length === 0) return 0;
+	return (arr.reduce((sum, current) => sum + current, 0)) / arr.length;
 }
